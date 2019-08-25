a25a845adcb5fe74a59355eb6fa091d94cf33a4f
dyt <741678393@qq.com>
2019-08-25 19:13:46 +0800

    添加data

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 ### 设计流程图
 ![Image text](./static/调起第三方地图.jpg)
 
+###  vue cli3.0用axios调用本地json数据一直报404
+最近在基于vue做后台管理系统时，用了vue cli3.0用axios调用本地json数据一直报404，市面上所有的解决办法都没用，快崩溃了，结果最后发现原因是，vue cli3.0 public 文件夹才是静态资源文件，问题解决，记录一下，以后不再踩坑。
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,25 @@
 /***
  * 欢迎页
  */
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      data1: null
+    };
+  },
+  methods: {
+    init() {
+      console.dir();
+      axios.get("/data/data.json").then(data => {
+        console.log(data);
+      });
+    }
+  },
+  created() {
+    this.init();
+  }
+};
 </script>
 <style lang="scss" scoped>
 </style>
